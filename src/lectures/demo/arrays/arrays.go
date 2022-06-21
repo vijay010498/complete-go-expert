4a9d77a79e2818756252bda9d6236427083ae0dc
vijay <vijayaraghavan16315@cse.ssn.edu.in>
2022-06-21 21:30:21 +0530

    array

--- src/lectures/demo/arrays/arrays.go
+++ src/lectures/demo/arrays/arrays.go
@@ -2,6 +2,32 @@ package main
 
 import "fmt"
 
-func main() {
+type Room struct {
+	name    string
+	cleaned bool
+}
 
+func checkCleanliness(rooms [4]Room) {
+	for i := 0; i < len(rooms); i++ {
+		room := rooms[i]
+		if room.cleaned {
+			fmt.Println(room.name, "is clean")
+		} else {
+			fmt.Println(room.name, "is not cleaned")
+		}
+	}
+}
+
+func main() {
+	rooms := [...]Room{
+		{name: "office"},
+		{name: "warehouse"},
+		{name: "Reception"},
+		{name: "Ops"},
+	}
+	checkCleanliness(rooms)
+
+	fmt.Println("Performing Cleaning...")
+	rooms[2].cleaned = true
+	rooms[3].cleaned = true
 }
--- src/lectures/exercise/arrays/arrays.go
+++ src/lectures/exercise/arrays/arrays.go
@@ -18,6 +18,32 @@ package main
 
 import "fmt"
 
-func main() {
+type Product struct {
+	name  string
+	price int
+}
 
+func printInformation(products [4]Product) {
+	var cost, totalItems int
+
+	for i := 0; i < len(products); i++ {
+		item := products[i]
+		cost += item.price
+
+		if item.name != "" {
+			totalItems += 1
+		}
+		fmt.Println("Last item:", products[totalItems-1])
+		fmt.Println("Total Items", totalItems)
+		fmt.Println("Total Cost", cost)
+	}
+}
+
+func main() {
+	products := [4]Product{
+		{name: "1", price: 10},
+		{name: "2", price: 10},
+		{name: "3", price: 10},
+	}
+	printInformation(products)
 }
