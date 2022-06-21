662627118f8022e03669678cd026d79204d87382
vijay <vijayaraghavan16315@cse.ssn.edu.in>
2022-06-21 17:21:05 +0530

    completed structures

--- src/lectures/demo/structs/structs.go
+++ src/lectures/demo/structs/structs.go
@@ -2,6 +2,35 @@ package main
 
 import "fmt"
 
-func main() {
+type Passenger struct {
+	Name         string
+	TicketNumber int
+	Boarded      bool
+}
 
+type Bus struct {
+	FrontSeat Passenger
+}
+
+func main() {
+	vijay := Passenger{"vijay", 1, false}
+	fmt.Println(vijay)
+
+	var (
+		pablo = Passenger{Name: "Pablo", TicketNumber: 2}
+	)
+
+	var neha Passenger
+	neha.Name = "Neha"
+	neha.TicketNumber = 4
+	fmt.Println(neha)
+
+	vijay.Boarded = true
+	pablo.Boarded = true
+
+	if vijay.Boarded {
+		fmt.Println(vijay.Name, "has Boarded the bus")
+	}
+	bus := Bus{vijay}
+	fmt.Println("Bus is here ", bus, "in front seat", bus.FrontSeat.Name)
 }
--- src/lectures/exercise/structs/structs.go
+++ src/lectures/exercise/structs/structs.go
@@ -19,6 +19,37 @@ package main
 
 import "fmt"
 
-func main() {
+type Coordinate struct {
+	x, y int
+}
 
+type Rectangle struct {
+	a Coordinate
+	b Coordinate
+}
+
+func width(rect Rectangle) int {
+	return rect.b.x - rect.a.x
+}
+
+func length(rect Rectangle) int {
+	return rect.a.y - rect.a.x
+}
+
+func area(rect Rectangle) int {
+	return length(rect) * width(rect)
+}
+
+func perimeter(rect Rectangle) int {
+	return (width(rect) * 2) + (length(rect) * 2)
+}
+
+func printInformation(rect Rectangle) {
+	fmt.Println("Area is ", area(rect))
+	fmt.Println("Perimeter  is ", perimeter(rect))
+
+}
+func main() {
+	rect := Rectangle{a: Coordinate{0, 7}, b: Coordinate{10, 0}}
+	printInformation(rect)
 }
