af4d8a76569fcbe640f9f38eb2f9610163a12ad4
vijay <vijayaraghavan16315@cse.ssn.edu.in>
2022-07-02 18:33:34 +0530

    done maps

--- src/lectures/exercise/maps/maps.go
+++ src/lectures/exercise/maps/maps.go
@@ -26,6 +26,46 @@ const (
 	Retired     = 3
 )
 
+func printServerStatus(servers map[string]int) {
+	fmt.Println("\n There are", len(servers), "servers")
+	stats := make(map[int]int)
+	for _, status := range servers {
+		switch status {
+		case Online:
+			stats[Online] += 1
+		case Offline:
+			stats[Offline] += 1
+		case Maintenance:
+			stats[Maintenance] += 1
+		case Retired:
+			stats[Retired] += 1
+		default:
+			panic("unhandled server status")
+		}
+	}
+	fmt.Println(stats[Online], "servers are online")
+	fmt.Println(stats[Offline], "servers are Offline")
+	fmt.Println(stats[Maintenance], "servers are Maintenance")
+	fmt.Println(stats[Retired], "servers are Retired")
+}
 func main() {
 	servers := []string{"darkstar", "aiur", "omicron", "w359", "baseline"}
+	serverStatus := make(map[string]int)
+
+	for _, server := range servers {
+		serverStatus[server] = Online
+	}
+
+	printServerStatus(serverStatus)
+	serverStatus["darkstar"] = Retired
+	serverStatus["aiur"] = Offline
+
+	printServerStatus(serverStatus)
+
+	for server := range serverStatus {
+		serverStatus[server] = Maintenance
+	}
+
+	printServerStatus(serverStatus)
+
 }
