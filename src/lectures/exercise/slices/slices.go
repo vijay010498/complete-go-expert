61abce331e87bf6213c0f93c59029cce761376d2
vijay <vijayaraghavan16315@cse.ssn.edu.in>
2022-07-02 15:57:50 +0530

    done slices

--- src/lectures/exercise/slices/slices.go
+++ src/lectures/exercise/slices/slices.go
@@ -18,5 +18,28 @@ import "fmt"
 
 type Part string
 
+func showLine(line []Part) {
+	for i := 0; i < len(line); i++ {
+		part := line[i]
+		fmt.Println(part)
+	}
+}
+
 func main() {
+	assemblyLine := make([]Part, 3)
+	assemblyLine[0] = "Pipe"
+	assemblyLine[1] = "Bolt"
+	assemblyLine[2] = "Sheet"
+
+	fmt.Println("3 Parts:")
+	showLine(assemblyLine)
+
+	assemblyLine = append(assemblyLine, "Washer", "Wheel")
+	fmt.Println("\n Added two parts:")
+	showLine(assemblyLine)
+
+	assemblyLine = assemblyLine[3:]
+	fmt.Println("\nSliced:")
+	showLine(assemblyLine)
+
 }
